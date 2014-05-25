31299a68078dbbc8224903b43bb98c4325285e88
root <root@alap.(none)>
2014-05-25 10:44:48 +0200

    Site logo, name and slogan rearranged

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -18,22 +18,21 @@
 
   <header class="header" id="header" role="banner">
   <div class="ss_row_wrapper">
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
+    <?php if ($logo || $site_name): ?>
+      <div class="header__logo-and-name" id="logo-and-name">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+        <?php endif; ?>
         <?php if ($site_name): ?>
           <h1 class="header__site-name" id="site-name">
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
           </h1>
         <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
       </div>
+    <?php endif; ?>
+
+    <?php if ($site_slogan): ?>
+      <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
     <?php endif; ?>
 
     <?php if ($secondary_menu): ?>
