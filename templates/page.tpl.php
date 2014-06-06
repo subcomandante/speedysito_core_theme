85a2d1d9657530bb2ab06c9db649e7af6cd3cd27
Michele Castigliego <michele.castigliego@altasis.it>
2014-06-06 16:49:38 +0200

    REGIONS  - Content bottom

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -115,6 +115,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
+      <?php print render($page['content_bottom']); ?>
       <?php print $feed_icons; ?>
     </div>
 
